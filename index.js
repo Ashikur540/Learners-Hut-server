60c69d6eea07889f71a027168567c3f769d11671
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-10-26 10:01:59 +0600

    basic api created

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
 
+// data
+const categoryData = require("./Data/categoryData.json");
+const coursesData = require("./Data/Courses.json");
 
 
 app.get("/", (req, res) => {
     res.send("Api is running...")
+})
+
+app.get("/courses/category/", (req, res) => {
+    res.send(categoryData)
+})
+app.get("/courses/category/:id", (req, res) => {
+    const c_id = req.params.id;
+    console.log(c_id);
+    const cat_coursesData = coursesData.filter(courses => courses.category_id === c_id);
+    console.log(cat_coursesData);
+    res.send(cat_coursesData);
 })
 
 app.listen(port, () => {
