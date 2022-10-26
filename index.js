122a5dd35bca6b96a06bfcafe590d45740bdd97d
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-10-26 11:34:06 +0600

    correcttions

--- index.js
+++ index.js
@@ -17,8 +17,15 @@ app.get("/courses/category/", (req, res) => {
     res.send(categoryData)
 })
 app.get("/courses/category/:id", (req, res) => {
+
     const c_id = req.params.id;
     console.log(c_id);
+
+
+    if (c_id === "08") {
+        res.send(coursesData);
+    }
+
     const cat_coursesData = coursesData.filter(courses => courses.category_id === c_id);
     console.log(cat_coursesData);
     res.send(cat_coursesData);
